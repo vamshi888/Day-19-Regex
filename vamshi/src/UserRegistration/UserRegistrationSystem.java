2dcf8bdb7d32c8c63021e9b14429cdc7938893b2
vamshi888 <vamshik484@gmail.com>
2022-05-18 12:44:24 +0530

    Details Validation

---
+++ vamshi/src/UserRegistration/UserRegistrationSystem.java
@@ -0,0 +1,142 @@
+package UserRegistration;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class UserRegistrationSystem {
+//Declaring Regex patterns
+    //UC1
+    public static final String firstName = "^[A-Z]{1}[a-z]{3,}$";
+    //UC2
+    public static final String lastName= "^[A-Z]{1}[a-z]{3,}$";
+    //UC3
+    public static final String emailRegexPattern = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
+    //String emailRegexPattern="^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
+    //UC4
+    public static final String mobileNoFormat = "^[9,1]{2}?[0-9]{10}$";
+    //UC5-Rule1
+    public static final String passwordRule1= "^[a-zA-Z0-9]{8,}$";
+          //    String regex = "^[A-Za-z]{8,}$";
+    //UC6-Rule2
+    public static final String passwordRule2 = "^[A-Z]{1,}[a-z]{7,}$";
+       //    String passwordPatternSecondRule="^[A-Z][a-zA-Z0-9]{8,}";
+    //UC7-Rule3
+    public static final String getPasswordRule3 = "^[A-Z]{1,}[0-9]{1,}[a-z]{6,}$";
+
+
+    //  UC1- method to validate first name
+    public static void validateFirstName() {
+        System.out.print("Enter First Name :- ");
+        Scanner sc = new Scanner(System.in);
+        String fistName = sc.nextLine();
+        Pattern pattern = Pattern.compile(firstName);
+        Matcher matcher = pattern.matcher(fistName);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("FirstName is Valid");
+        } else {
+            System.out.println("FirstName is Invalid");
+        }
+    }
+
+    //  UC2- method to validate last name
+    public static void validateLastName() {
+        System.out.print("Enter Last Name :- ");
+        Scanner sc = new Scanner(System.in);
+        String lastName = sc.nextLine();
+        Pattern pattern = Pattern.compile(lastName);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("LastName is Valid");
+        } else {
+            System.out.println("LastName is Invalid");
+        }
+    }
+
+    //  UC3- method to validate mail
+    public static void validateMail() {
+        System.out.print("Enter your Mail ID :- ");
+        Scanner sc = new Scanner(System.in);
+        String eMail = sc.nextLine();
+        Pattern pattern = Pattern.compile(emailRegexPattern);
+        Matcher matcher = pattern.matcher(eMail);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("Mail ID is Valid");
+        } else {
+            System.out.println("Mail ID is Invalid");
+        }
+    }
+
+    //  UC4- method to validate mobile number
+    public static void validateMobileNumber() {
+        System.out.print("Enter Mobile Number :- ");
+        Scanner sc = new Scanner(System.in);
+        String mobNum = sc.next();
+        Pattern pattern = Pattern.compile(mobileNoFormat);
+        Matcher matcher = pattern.matcher(mobNum);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("Mobile Number is Valid");
+        } else {
+            System.out.println("Mobile Number is Invalid");
+        }
+    }
+
+
+    // UC5- Rule1-method to validate password of minimum 8 characters
+    public static void validatePasswordRule1() {
+        System.out.print("Enter Password :- ");
+        Scanner sc = new Scanner(System.in);
+        String passWord = sc.next();
+        Pattern pattern = Pattern.compile(passwordRule1);
+        Matcher matcher = pattern.matcher(passWord);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("Password is Valid");
+        } else {
+            System.out.println("Password is Invalid");
+        }
+    }
+
+    // UC6- Rule2-method to validate password of minimum 1 UpperCase
+    public static void validatePasswordRule2() {
+        System.out.print("Enter Password :- ");
+        Scanner sc = new Scanner(System.in);
+        String passWord = sc.next();
+        Pattern pattern = Pattern.compile(passwordRule2);
+        Matcher matcher = pattern.matcher(passWord);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("Password is Valid");
+        } else {
+            System.out.println("Password is Invalid");
+        }
+    }
+
+    // UC7- Rule3-method to validate password of minimum 1 numeric character
+    public static void validatePasswordRule3() {
+        System.out.print("Enter Password :- ");
+        Scanner sc = new Scanner(System.in);
+        String passWord = sc.next();
+        Pattern pattern = Pattern.compile(getPasswordRule3);
+        Matcher matcher = pattern.matcher(passWord);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("Password is Valid");
+        } else {
+            System.out.println("Password is Invalid");
+        }
+    }
+
+        public static void main(String args[]) {
+            System.out.println("Welcome to User Registration System Program");
+            validateFirstName();
+            validateLastName();
+            validateMail();
+            validateMobileNumber();
+            validatePasswordRule1();
+            validatePasswordRule2();
+            validatePasswordRule3();
+    }
+ }
